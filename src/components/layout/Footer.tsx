89f7876e5ab3c25c786e673e46447c8e35a5778c
q1mlvd <mishakravhuk@gmail.com>
2026-03-05 23:13:13 +0100

    Fix typo in footer copyright name

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -41,7 +41,7 @@ export function Footer() {
           </div>
 
           <p className="text-sm text-gray-500 dark:text-gray-500">
-            © {new Date().getFullYear()} Mikhailo Kravchuk. {t("rights")}
+            © {new Date().getFullYear()} Mikhailo Kravchu1k. {t("rights")}
           </p>
         </div>
       </div>
